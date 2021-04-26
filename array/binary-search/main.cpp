dbdfa752e2abc9bb63518d76e9803ec7ac06c795
Li Tian <li_tian@singaporeair.com.sg>
2021-04-26 17:34:20 +0800

    array: #binary-search #704, kick off

---
+++ array/binary-search/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+
+class Solution {
+	public:
+};
+
+
+int main() {
+    Soultion s = Soultion();
+	return 0;
+}
