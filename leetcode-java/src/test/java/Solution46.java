49eee484907224e2e4fec46082445defad2d7ed3
hantv-dta <ha.nguyen@digitran.asia>
2022-07-04 12:51:50 +0700

    problems 46 java solution

---
+++ leetcode-java/src/test/java/Solution46.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution46 {
+
+    public List<List<Integer>> answers = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        boolean[] visit = new boolean[nums.length];
+        List<Integer> permutation = new ArrayList<>();
+        dfs(nums, permutation, visit);
+        return answers;
+    }
+
+    private void dfs(int[] nums, List<Integer> permutation, boolean[] visit) {
+        if (permutation.size() == nums.length) {
+            answers.add(new ArrayList<>(permutation));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!visit[i]) {
+                permutation.add(nums[i]);
+                visit[i] = true;
+                dfs(nums, permutation, visit);
+                visit[i] = false;
+            }
+        }
+    }
+}
--- leetcode-java/src/test/java/Solution46Test.java
+++ leetcode-java/src/test/java/Solution46Test.java
@@ -1,11 +1,10 @@
-import com.sun.java.swing.plaf.windows.WindowsTextAreaUI;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class Solution46Test {
     Solution46 sln;
@@ -17,11 +16,28 @@ class Solution46Test {
 
     @Test
     void permute() {
-        List<Integer> nums = Arrays.asList(1,2,3);
+        int[] nums = {1, 2, 3};
         List<List<Integer>> ans = sln.permute(nums);
-        List<List<Integer>> expect = Arrays.asList()
+        List<List<Integer>> expect = new ArrayList<>();
+        expect.add(Arrays.asList(1,2,3));
+        expect.add(Arrays.asList(1,3,2));
+        expect.add(Arrays.asList(2,1,3));
+        expect.add(Arrays.asList(2,3,1));
+        expect.add(Arrays.asList(3,1,2));
+        expect.add(Arrays.asList(3,2,1));
+        Assertions.assertEquals(ans.size(), factorial(nums.length));
+        Assertions.assertEquals(ans, expect );
 
+        for(List<Integer> perm:ans) {
+            System.out.println(perm);
+        }
+    }
 
-
+    private int factorial (int num) {
+        int res = 1;
+        for(int i = 1; i <= num; i++) {
+            res *= i;
+        }
+        return res;
     }
 }
